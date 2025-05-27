f3d7a0e5a6094b298997a2aab1441028d393ec0f
Arthuricaroo <arthur.icaro@escolar.ifrn.edu.br>
2025-05-27 17:54:13 -0300

    Att2

---
+++ PinballCerto/Assets/esquedo.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class LadoEsquerdo : MonoBehaviour
+{
+   
+    private HingeJoint2D hinge;
+    private JointMotor2D motor;
+
+    public float speed = 1000f;
+
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint2D>();
+        motor = hinge.motor;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            motor.motorSpeed = -speed;
+            hinge.motor = motor;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            motor.motorSpeed = speed;
+            hinge.motor = motor;
+        }
+    }
+}
